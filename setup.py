e5c11dd13f76cff2dfa4d990b13a19bdfe0644c3
Krystian Jandy <krystianj@meritus.pl>
2024-05-04 13:54:54 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import Extension
 
 setup(
     name='simple_graphs',
-    version='1.0',
+    version='1.0.0',
+    author='Krystian Jandy, s184589',
+    author_email='s184589@student.pg.edu.pl',
+    url='https://github.com/Krystian030/simple-graphs',
     ext_modules=[Extension('simple_graphs', ['simple_graphs.c'])],
 )
